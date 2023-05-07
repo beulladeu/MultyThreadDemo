e902b2502a8c5771c5e86c2a2507b16f7f98157a
Владислава <v.verhonina@bftcom.com>
2023-05-08 02:46:34 +0300

    reworking

--- src/main/java/com/verkhonina/multythreaddemo/Foo.java
+++ src/main/java/com/verkhonina/multythreaddemo/Foo.java
@@ -1,17 +1,27 @@
 package main.java.com.verkhonina.multythreaddemo;
 
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Foo {
 
-    public void first(Runnable r) {
+    private final AtomicBoolean bManager = new AtomicBoolean();
+    private final AtomicBoolean cManager = new AtomicBoolean();
+
+    public synchronized void first(Runnable r) {
         r.run();
+        bManager.set(true);
+        notifyAll();
     }
 
-    public void second(Runnable r) {
+    public synchronized void second(Runnable r) throws InterruptedException {
+        while (!bManager.get()) wait();
         r.run();
+        cManager.set(true);
+        notify();
     }
 
-    public void third(Runnable r) {
+    public synchronized void third(Runnable r) throws InterruptedException {
+        while (!cManager.get()) wait();
         r.run();
     }
 }
--- src/main/java/com/verkhonina/multythreaddemo/Main.java
+++ src/main/java/com/verkhonina/multythreaddemo/Main.java
@@ -2,23 +2,29 @@ package main.java.com.verkhonina.multythreaddemo;
 
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Foo f = new Foo();
 
         Thread a = new Thread(() -> f.first(() -> System.out.print("first")));
 
-        Thread b = new Thread(() -> f.second(() -> System.out.print("second")));
+        Thread b = new Thread(() -> {
+            try {
+                f.second(() -> System.out.print("second"));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
 
-        Thread c = new Thread(() -> f.third(() -> System.out.print("third")));
-
-        a.start();
-        a.join();
+        Thread c = new Thread(() -> {
+            try {
+                f.third(() -> System.out.print("third"));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
 
         b.start();
-        b.join();
-
+        a.start();
         c.start();
-        c.join();
-
     }
 }
